05c941f596e159f3e8204d3b853f305618c947d8
Gene Clark <gene.r.clark@gmail.com>
2019-03-12 16:58:25 -0400

    name change

--- devicetypes/smartweather_v3.groovy
+++ devicetypes/smartweather_v3.groovy
@@ -17,7 +17,7 @@
  *  Date: 2013-04-30
  */
 metadata {
-    definition (name: "SmartWeather Station Tile", namespace: "timeteo", author: "Timeteo") {
+    definition (name: "SmartWeather v3", namespace: "timeteo", author: "Timeteo") {
         capability "Illuminance Measurement"
         capability "Temperature Measurement"
         capability "Relative Humidity Measurement"
